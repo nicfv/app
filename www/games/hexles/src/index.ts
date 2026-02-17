b2f17a3266eee6174d8866f39b212d8530ce1b9d
Nic <github@nicfv.com>
2026-02-16 17:22:33 -0800

    move interaction funcs into new canvas

--- www/games/hexles/src/index.ts
+++ www/games/hexles/src/index.ts
@@ -7,47 +7,43 @@ const CANV: Canvas = new Canvas({
     background: 'cornflowerblue',
     showMouse: false,
     keepFocused: true,
-    keydown(key) { onkeydown(key) },
-    loop(dt) { tick(dt) },
+    keydown(key) {
+        switch (key.toLowerCase()) {
+            case ('arrowup'):
+            case ('w'): {
+                Hexles.receiveInput('up');
+                break;
+            }
+            case ('arrowdown'):
+            case ('s'): {
+                Hexles.receiveInput('down');
+                break;
+            }
+            case ('arrowleft'):
+            case ('a'): {
+                Hexles.receiveInput('CCW');
+                break;
+            }
+            case ('arrowright'):
+            case ('d'): {
+                Hexles.receiveInput('CW');
+                break;
+            }
+            case ('enter'):
+            case (' '): {
+                Hexles.receiveInput('select');
+                break;
+            }
+            case ('backspace'):
+            case ('escape'): {
+                Hexles.receiveInput('back');
+                break;
+            }
+        }
+    },
+    loop(dt) {
+        CANV.clear();
+        Hexles.advance(dt);
+        CANV.draw(Hexles.handle());
+    },
 });
-
-const tick = (dt: number) => {
-    CANV.clear();
-    Hexles.advance(dt);
-    CANV.draw(Hexles.handle());
-};
-
-const onkeydown = (key: string) => {
-    switch (key.toLowerCase()) {
-        case ('arrowup'):
-        case ('w'): {
-            Hexles.receiveInput('up');
-            break;
-        }
-        case ('arrowdown'):
-        case ('s'): {
-            Hexles.receiveInput('down');
-            break;
-        }
-        case ('arrowleft'):
-        case ('a'): {
-            Hexles.receiveInput('CCW');
-            break;
-        }
-        case ('arrowright'):
-        case ('d'): {
-            Hexles.receiveInput('CW');
-            break;
-        }
-        case ('enter'):
-        case (' '): {
-            Hexles.receiveInput('select');
-            break;
-        }
-        case ('backspace'):
-        case ('escape'): {
-            Hexles.receiveInput('back');
-            break;
-        }
-    }
-};
