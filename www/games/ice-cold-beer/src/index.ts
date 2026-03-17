5fd24edf9a8e9b01bf2bad60e9a687937b6391f5
nicfv <github@nicfv.com>
2026-03-16 18:31:06 -0700

    test logic to generate holes

--- www/games/ice-cold-beer/src/index.ts
+++ www/games/ice-cold-beer/src/index.ts
@@ -2,11 +2,33 @@ import { Canvas } from 'graphico';
 import { Ball } from './ball';
 import { Rod } from './rod';
 import { Hole } from './hole';
+import { rint } from 'smath';
 
-const ball = new Ball(0, 0, 20, 400);
-const rod = new Rod(550, 550, 400, 600);
-const hole = new Hole(50, 50, 20);
-hole.select();
+const gameWidth = 400,
+    gameHeight = 600,
+    ballSize = 10,
+    padding = 2;
+
+const ball = new Ball(0, 0, ballSize, gameWidth);
+const rod = new Rod(gameHeight - 50, gameHeight - 50, gameWidth, gameHeight);
+const holes: Array<Hole> = [];
+
+const holeR = ballSize * 1.1;
+for (let y = gameHeight - 100; y > holeR * 2; y -= rint(1, holeR)) {
+    const hole = new Hole(rint(holeR * 2, gameWidth - holeR * 2), y, holeR, padding);
+    let intersects = false;
+    for (const existingHole of holes) {
+        if (hole.intersects(existingHole)) {
+            console.log('!');
+            intersects = true;
+            break;
+        }
+    }
+    if (!intersects) {
+        holes.push(hole);
+    }
+}
+holes[0].select();
 
 const canv = new Canvas({
     background: 'sandybrown',
@@ -21,7 +43,9 @@ const canv = new Canvas({
             canv.isKeyDown('w') ? 'Up' : canv.isKeyDown('s') ? 'Down' : 'None',
             canv.isKeyDown('i') ? 'Up' : canv.isKeyDown('k') ? 'Down' : 'None');
         ball.move(dt, rod);
-        canv.draw(hole);
+        for (const hole of holes) {
+            canv.draw(hole);
+        }
         canv.draw(ball);
         canv.draw(rod);
     },
