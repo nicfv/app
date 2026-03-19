8fbd1a0dbc633f65088535b50968f4a96585ff13
nicfv <github@nicfv.com>
2026-03-18 17:45:36 -0700

    motion calculations

--- www/games/ice-cold-beer/src/ball.ts
+++ www/games/ice-cold-beer/src/ball.ts
@@ -3,9 +3,12 @@ import { Rod } from './rod';
 import { Hole } from './hole';
 
 export class Ball implements Drawable {
+    private rolling = true;
     private vx = 0;
+    private vy = 0;
+    private airResistance = 0.99;
     private near = false; // near to a hole?
-    private readonly maxvx = 100; // max vx to fall into hole
+    private readonly maxvx = 10; // max vx to fall into hole
     private readonly maxdf = 0.5; // max distance factor
     /**
      * Create a new ball.
@@ -20,9 +23,20 @@ export class Ball implements Drawable {
      * Move the ball.
      */
     public move(dt: number, rod: Rod, holes: Hole[]): void {
-        // Calculate the acceleration and position of the marble
-        this.vx += Math.sin(rod.getAngle()) * this.g * dt / 1e3;
+        // Calculate acceleration
+        if (this.rolling) {
+            // Calculate the acceleration and position of the marble
+            this.vx += Math.sin(rod.getAngle()) * this.g * dt / 1e3;
+        } else {
+            // Ball is in the air
+            this.vx *= this.airResistance;
+            this.vy += this.g * dt / 1e3;
+            this.y += this.vy * dt / 1e3;
+        }
+        // Laws of motion
         this.x += this.vx * dt / 1e3;
+        this.y += this.vy * dt / 1e3;
+        // Game boundary
         if (this.x <= this.r) {
             // Hit left side
             this.x = this.r;
@@ -32,13 +46,23 @@ export class Ball implements Drawable {
             this.x = this.gameWidth - this.r;
             this.vx = 0;
         }
-        // Assume ball is "stuck" to the rod
-        this.y = rod.getY(this.x) - (this.r / Math.cos(rod.getAngle())) - rod.width / 2;
+        // Calculate the height of the rod at the ball's x-coordinate
+        const rodY: number = rod.getY(this.x) - (this.r / Math.cos(rod.getAngle())) - rod.width / 2;
+        if (this.rolling) {
+            // Assume ball is "stuck" to the rod
+            this.y = rodY;
+        } else if (this.y > rodY) {
+            // Make sure that ball hasn't "phased-through" the rod
+            this.y = rodY;
+            this.vy = 0;
+            this.rolling = true;
+        }
+        // Check to see if this ball would fall into any holes
         let latch = false;
         for (const hole of holes) {
             if (hole.distanceFrom(this.x, this.y) < this.r * this.maxdf) {
                 latch = true;
-                if (!this.near) {
+                if (!this.near && this.rolling) {
                     if (Math.abs(this.vx) < this.maxvx) {
                         console.log('you win!');
                         this.near = true;
@@ -46,10 +70,13 @@ export class Ball implements Drawable {
                         console.log('not yet buster');
                         this.near = true;
                         this.vx /= 2;
+                        this.vy = -Math.abs(this.vx);
+                        this.rolling = false;
                     }
                 }
             }
         }
+        // Unlatch proximity to hole
         if (!latch) {
             this.near = false;
         }
