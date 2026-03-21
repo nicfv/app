e87d3eb81e8363b9cd2480b47fad76369e372c9c
nicfv <github@nicfv.com>
2026-03-21 14:53:32 -0700

    draw game entities

--- www/games/ice-cold-beer/src/game.ts
+++ www/games/ice-cold-beer/src/game.ts
@@ -2,17 +2,19 @@ import { Drawable } from 'graphico';
 import { Ball } from './ball';
 import { Rod } from './rod';
 import { Hole } from './hole';
+import { rint } from 'smath';
 
 export class Game implements Drawable {
     private readonly ball: Ball;
     private readonly rod: Rod;
     private readonly holes: Hole[];
-    constructor(width: number, height: number, difficulty: 'Easy' | 'Medium' | 'Hard') {
-        const ballR = 20;
+    constructor(public readonly width: number, public readonly height: number, difficulty: 'Easy' | 'Medium' | 'Hard') {
+        const ballR = 10;
         let gFac = 1;
         let maxSpeed = 100;
         let maxDistFac = 0.25;
         let holeRFac = 1.1;
+        let holePadding = 1.75;
         let showSpeed = false;
         switch (difficulty) {
             case ('Easy'): {
@@ -20,6 +22,7 @@ export class Game implements Drawable {
                 maxDistFac = 0.30;
                 maxSpeed = 200;
                 holeRFac = 1.5;
+                showSpeed = true;
                 break;
             }
             case ('Medium'): {
@@ -34,10 +37,27 @@ export class Game implements Drawable {
         }
         this.ball = new Ball(0, 0, ballR, width, 9.81 * 500 * gFac, 0.99, maxSpeed, maxDistFac, 0.33, showSpeed);
         this.rod = new Rod(height - 100, height - 100, width, height, 100, 10);
+        const holeR: number = ballR * holeRFac;
         this.holes = [];
+        for (let y = height - 100; y > holeR * 2; y -= rint(1, holeR)) {
+            const hole = new Hole(rint(holeR * 2, width - holeR * 2), y, holeR, holePadding);
+            let intersects = false;
+            for (const existingHole of this.holes) {
+                if (hole.intersects(existingHole)) {
+                    intersects = true;
+                    break;
+                }
+            }
+            if (!intersects) {
+                this.holes.push(hole);
+            }
+        }
     }
     public draw(graphics: CanvasRenderingContext2D): void {
-        console.log(graphics);
-        throw new Error('Method not implemented.');
+        for (const hole of this.holes) {
+            hole.draw(graphics);
+        }
+        this.ball.draw(graphics);
+        this.rod.draw(graphics);
     }
 }
--- www/games/ice-cold-beer/src/index.ts
+++ www/games/ice-cold-beer/src/index.ts
@@ -1,51 +1,21 @@
 import { Canvas } from 'graphico';
-import { Ball } from './ball';
-import { Rod } from './rod';
-import { Hole } from './hole';
-import { rint } from 'smath';
+import { Game } from './game';
 
-const gameWidth = 400,
-    gameHeight = 600,
-    ballSize = 10,
-    padding = 2;
-
-const ball = new Ball(0, 0, ballSize, gameWidth, 9.81 * 500, 0.99, 100, 0.25, 0.4, true);
-const rod = new Rod(gameHeight - 50, gameHeight - 50, gameWidth, gameHeight);
-const holes: Hole[] = [];
-
-const holeR = ballSize * 1.1;
-for (let y = gameHeight - 100; y > holeR * 2; y -= rint(1, holeR)) {
-    const hole = new Hole(rint(holeR * 2, gameWidth - holeR * 2), y, holeR, padding);
-    let intersects = false;
-    for (const existingHole of holes) {
-        if (hole.intersects(existingHole)) {
-            intersects = true;
-            break;
-        }
-    }
-    if (!intersects) {
-        holes.push(hole);
-    }
-}
-holes[0].select();
+const game = new Game(400, 600, 'Easy');
 
 const canv = new Canvas({
     background: 'sandybrown',
     border: 'black',
     borderBlur: 'white',
     showMouse: false,
-    height: gameHeight,
-    width: gameWidth,
+    height: game.height,
+    width: game.width,
     loop(dt) {
         canv.clear();
-        rod.move(dt,
-            canv.isKeyDown('w') ? 'Up' : canv.isKeyDown('s') ? 'Down' : 'None',
-            canv.isKeyDown('i') ? 'Up' : canv.isKeyDown('k') ? 'Down' : 'None');
-        ball.move(dt, rod, holes);
-        for (const hole of holes) {
-            canv.draw(hole);
-        }
-        canv.draw(ball);
-        canv.draw(rod);
+        // rod.move(dt,
+        //     canv.isKeyDown('w') ? 'Up' : canv.isKeyDown('s') ? 'Down' : 'None',
+        //     canv.isKeyDown('i') ? 'Up' : canv.isKeyDown('k') ? 'Down' : 'None');
+        // ball.move(dt, rod, holes);
+        canv.draw(game);
     },
 });
