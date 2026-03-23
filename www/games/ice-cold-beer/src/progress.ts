d04380d406b32986aaeb8f832f32044c79a8a7d4
nicfv <github@nicfv.com>
2026-03-22 19:46:43 -0700

    add constructor and markCompleted func

--- www/games/ice-cold-beer/src/progress.ts
+++ www/games/ice-cold-beer/src/progress.ts
@@ -1,6 +1,11 @@
 import { Drawable } from 'graphico';
 
 export class Progress implements Drawable {
+    private completed = false;
+    constructor(private readonly x: number, private readonly y: number, private readonly r: number) { }
+    public markComplete(): void {
+        this.completed = true;
+    }
     public draw(graphics: CanvasRenderingContext2D): void {
         throw new Error("Method not implemented.");
     }
