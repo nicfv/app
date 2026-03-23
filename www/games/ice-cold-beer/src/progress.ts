5444924189e7ac7bbb65aa6c86ff70ecba7350c3
nicfv <github@nicfv.com>
2026-03-22 19:47:50 -0700

    docs

--- www/games/ice-cold-beer/src/progress.ts
+++ www/games/ice-cold-beer/src/progress.ts
@@ -1,8 +1,20 @@
 import { Drawable } from 'graphico';
 
+/**
+ * A progress indicator to show how far you've gone through the game
+ */
 export class Progress implements Drawable {
+    /**
+     * Whether or not this has been marked as completed
+     */
     private completed = false;
+    /**
+     * Create a new progress indicator
+     */
     constructor(private readonly x: number, private readonly y: number, private readonly r: number) { }
+    /**
+     * Mark this progress indicator as completed
+     */
     public markComplete(): void {
         this.completed = true;
     }
