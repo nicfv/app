f38197b81d3229b9e8329c8ed1f4b7232d535198
nicfv <github@nicfv.com>
2026-03-27 14:27:36 -0700

    testing out menu

--- www/games/ice-cold-beer/src/index.ts
+++ www/games/ice-cold-beer/src/index.ts
@@ -1,7 +1,9 @@
 import { Canvas } from 'graphico';
 import { Game } from './game';
+import { Menu } from './menu';
 
-const game = new Game(400, 600, 'X-treme');
+const game = new Game(400, 600, 'Easy');
+const menu = new Menu('Ballistic', 'Select a difficulty and\npress Enter to start:', ['Easy', 'Medium', 'Hard']);
 
 const canv = new Canvas({
     background: 'sandybrown',
@@ -15,5 +17,14 @@ const canv = new Canvas({
         game.input(canv.isKeyDown('w'), canv.isKeyDown('s'), canv.isKeyDown('i'), canv.isKeyDown('k'));
         game.tick(dt);
         canv.draw(game);
+        canv.clear();
+        canv.draw(menu);
+    },
+    keydown(key) {
+        if (key === 'w' || key === 'arrowup') {
+            menu.scrollUp();
+        } else if (key === 's' || key === 'arrowdown') {
+            menu.scrollDown();
+        }
     },
 });
