0b7f1ac9a52ed8070700672ecf6efbb7969759c4
nicfv <github@nicfv.com>
2026-03-27 13:28:30 -0700

    add font getter

--- www/games/ice-cold-beer/src/menu.ts
+++ www/games/ice-cold-beer/src/menu.ts
@@ -2,12 +2,18 @@ import { Drawable } from 'graphico';
 
 export class Menu implements Drawable {
     constructor(private readonly width: number, private readonly height: number) { }
+    /**
+     * Get the font string for a given size.
+     */
+    private font(size: number): string {
+        return `bold ${size}px monospace`;
+    }
     public draw(graphics: CanvasRenderingContext2D): void {
-        graphics.fillStyle = 'black';
-        graphics.font = '48px sans-serif';
+        graphics.fillStyle = 'white';
+        graphics.font = this.font(48);
         graphics.textAlign = 'center';
         graphics.fillText('Ice Cold Beer', this.width / 2, this.height / 2 - 50);
-        graphics.font = '24px sans-serif';
+        graphics.font = this.font(24);
         graphics.fillText('Press any key to start', this.width / 2, this.height / 2 + 20);
     }
 }
