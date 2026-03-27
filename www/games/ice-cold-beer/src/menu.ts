64e609852814411bad61d101c2e3cdaca996b54d
nicfv <github@nicfv.com>
2026-03-27 14:12:01 -0700

    vibe code some menu options

--- www/games/ice-cold-beer/src/menu.ts
+++ www/games/ice-cold-beer/src/menu.ts
@@ -1,19 +1,38 @@
 import { Drawable } from 'graphico';
 
-export class Menu implements Drawable {
-    constructor(private readonly width: number, private readonly height: number) { }
+export class Menu<T extends string> implements Drawable {
+    private selection = 0;
+    constructor(private readonly title: string, private readonly description: string, private readonly options: T[]) { }
     /**
      * Get the font string for a given size.
      */
     private font(size: number): string {
         return `bold ${size}px monospace`;
     }
+    /**
+     * Scroll the menu up, changing the selection.
+     */
+    public scrollUp(): void {
+        this.selection = (this.selection - 1 + this.options.length) % this.options.length;
+    }
+    /**
+     * Scroll the menu down, changing the selection.
+     */
+    public scrollDown(): void {
+        this.selection = (this.selection + 1) % this.options.length;
+    }
+    /**
+     * Get the currently selected option.
+     */
+    public select(): T {
+        return this.options[this.selection];
+    }
     public draw(graphics: CanvasRenderingContext2D): void {
         graphics.fillStyle = 'white';
-        graphics.font = this.font(48);
         graphics.textAlign = 'center';
-        graphics.fillText('Ice Cold Beer', this.width / 2, this.height / 2 - 50);
+        graphics.font = this.font(48);
+        graphics.fillText(this.title, graphics.canvas.width / 2, graphics.canvas.height / 2 - 50);
         graphics.font = this.font(24);
-        graphics.fillText('Press any key to start', this.width / 2, this.height / 2 + 20);
+        graphics.fillText(this.description, graphics.canvas.width / 2, graphics.canvas.height / 2 + 20);
     }
 }
