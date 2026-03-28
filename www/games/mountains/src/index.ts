19d5efd70d152290b52420877c40bc7539ec8f64
Nic <github@nicfv.com>
2026-03-28 16:32:52 -0700

    delete mountains

--- www/games/mountains/src/index.ts
+++
@@ -1,7 +0,0 @@
-import { layers, mounts } from './game';
-import { Mountain } from './mount';
-import * as SMath from 'smath';
-
-for (let i = 0; i < layers; i++) {
-    mounts.push(new Mountain(SMath.normalize(i, layers - 1, 0)));
-}
