2e03e6bfc1c529439c18790c8ba21186bc1ec60d
Nic <github@nicfv.com>
2026-03-28 16:56:59 -0700

    cell draw

--- www/games/cellsplit/src/cell.ts
+++ www/games/cellsplit/src/cell.ts
@@ -1,7 +1,19 @@
 import { Drawable } from 'graphico';
+import { Vec2 } from './types';
+import { cellSize, gridSize, pan } from './globals';
 
 export class Cell implements Drawable {
+    private hover: boolean;
+    constructor(private readonly gridPos: Vec2) {
+        this.hover = false;
+    }
     public draw(graphics: CanvasRenderingContext2D): void {
-        throw new Error('Method not implemented.');
+        graphics.fillStyle = 'lime';
+        graphics.strokeStyle = 'green';
+        graphics.lineWidth = 2;
+        graphics.beginPath();
+        graphics.arc(pan.x + this.gridPos.x * gridSize.x, pan.y + this.gridPos.y + gridSize.y, cellSize, 0, 2 * Math.PI);
+        graphics.fill();
+        graphics.stroke();
     }
 }
