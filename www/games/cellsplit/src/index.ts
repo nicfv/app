fe59fcf4a3be0932f354ca5f10c47fb8636d1495
Nic <github@nicfv.com>
2026-03-28 16:42:40 -0700

    import canvas

--- www/games/cellsplit/src/index.ts
+++ www/games/cellsplit/src/index.ts
@@ -1,1 +1,14 @@
-console.log('Hello!');
+import { Canvas } from 'graphico';
+
+const canv = new Canvas({
+    background: 'lightgray',
+    border: 'black',
+    borderBlur: 'gray',
+    width: 800,
+    height: 600,
+    loop(dt) {
+        if (canv.isMouseButtonDown(dt)) {
+            console.log('Life is meaningless.');
+        }
+    },
+});
