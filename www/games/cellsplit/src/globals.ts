1af8f94ef3f63e9229df01177963926ae6080096
Nic <github@nicfv.com>
2026-03-28 16:52:18 -0700

    comments!

--- www/games/cellsplit/src/globals.ts
+++ www/games/cellsplit/src/globals.ts
@@ -1,4 +1,10 @@
 import { Vec2 } from './types';
 
+/**
+ * Camera pan [px]
+ */
 export const pan: Vec2 = { x: 0, y: 0 };
+/**
+ * World grid size [px]
+ */
 export const gridSize: Vec2 = { x: 10, y: 10 };
