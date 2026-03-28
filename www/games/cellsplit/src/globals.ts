b1809cc6002c938aff3cf027a37377eacf9b86f2
Nic <github@nicfv.com>
2026-03-28 16:56:47 -0700

    set cell radius

--- www/games/cellsplit/src/globals.ts
+++ www/games/cellsplit/src/globals.ts
@@ -8,3 +8,7 @@ export const pan: Vec2 = { x: 0, y: 0 };
  * World grid size [px]
  */
 export const gridSize: Vec2 = { x: 10, y: 10 };
+/**
+ * Cell radius [px]
+ */
+export const cellSize: number = 5;
