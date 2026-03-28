48794401d7ea9a4c9db0fd58eb78e21f8bd3f9f5
Nic <24358775+nicfv@users.noreply.github.com>
2026-03-27 20:29:56 -0700

    Ball (#10)
    v1.0.1

---
+++ www/games/ice-cold-beer/src/ball.ts
@@ -0,0 +1,44 @@
+import { Drawable } from 'graphico';
+import { Rod } from './rod';
+
+export class Ball implements Drawable {
+    private freeFall = true;
+    private vx = 0;
+    private vy = 0;
+    private readonly g: number = 9.81 * 100;
+    constructor(private x: number, private y: number, private r: number) { }
+    public move(dt: number, rod: Rod): void {
+        const maxY: number = rod.getY(this.x) - this.r - rod.width / 2;
+        if (this.freeFall) {
+            this.vy += this.g * dt / 1e3;
+            this.y += this.vy * dt / 1e3;
+            if (this.y > maxY) {
+                this.y = maxY;
+                this.freeFall = false;
+            }
+        } else {
+            this.y = maxY;
+        }
+    }
+    public draw(graphics: CanvasRenderingContext2D): void {
+        // Marble
+        graphics.beginPath();
+        graphics.arc(this.x, this.y, this.r, 0, 2 * Math.PI);
+        graphics.fillStyle = 'darkgray';
+        graphics.fill();
+        // Outline
+        graphics.strokeStyle = 'gray';
+        graphics.lineWidth = 2;
+        graphics.stroke();
+        // Large reflection
+        graphics.beginPath();
+        graphics.arc(this.x + this.r / 2, this.y + this.r / 2, this.r / 6, 0, 2 * Math.PI);
+        graphics.fillStyle = 'lightgray';
+        graphics.fill();
+        // Small reflection
+        graphics.beginPath();
+        graphics.arc(this.x - this.r / 3, this.y - this.r / 3, this.r / 3, 0, 2 * Math.PI);
+        graphics.fillStyle = 'lightgray';
+        graphics.fill();
+    }
+}
---
+++ www/games/ice-cold-beer/src/index.ts
@@ -0,0 +1,21 @@
+import { Canvas } from 'graphico';
+import { Ball } from './ball';
+import { Rod } from './rod';
+
+const ball = new Ball(100, 100, 20);
+const rod = new Rod(0, 550, 400, 550);
+
+const canv = new Canvas({
+    background: 'sandybrown',
+    border: 'black',
+    borderBlur: 'white',
+    showMouse: false,
+    height: 600,
+    width: 400,
+    loop(dt) {
+        canv.clear();
+        ball.move(dt, rod);
+        canv.draw(ball);
+        canv.draw(rod);
+    },
+});
