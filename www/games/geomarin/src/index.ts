b0ea077d3730ba5eea477f0aabf80d926e99770e
nicfv <github@nicfv.com>
2026-05-15 14:38:58 -0700

    import svg as a text module, and add some code comments

--- www/games/geomarin/src/index.ts
+++ www/games/geomarin/src/index.ts
@@ -1,26 +1,32 @@
 import globals from './globals';
 import * as Lib from './lib';
+import svg from '../assets/marin.svg' with { type: 'text' };
 
-window.addEventListener('load', main);
+// Get DOM elements
+const mapContainer: HTMLObjectElement = document.getElementById('marin-map') as HTMLObjectElement;
+const buttonContainer: HTMLDivElement = document.getElementById('buttons') as HTMLDivElement;
 
-function main(): void {
-  console.log('Loaded!', Lib.getDate());
-  const map: HTMLObjectElement = document.getElementById('marin-map') as HTMLObjectElement,
-    buttonContainer: HTMLDivElement = document.getElementById('buttons') as HTMLDivElement,
-    doc: Document = map.contentDocument as Document,
-    paths: SVGElement[] = Array.from(doc.getElementsByTagNameNS('http://www.w3.org/2000/svg', 'path')),
-    filteredPaths: SVGElement[] = paths
-      .filter(path => !globals.staticPathNames.includes(path.id))
-      .sort((a, b) => a.id.localeCompare(b.id)),
-    title: SVGTextElement = Lib.createText('white', 0.75, 10, 290, 'start', 'alphabetic');
-  doc.firstChild?.appendChild(title);
-  filteredPaths.forEach(path => {
-    const button = document.createElement('div');
-    button.setAttribute('class', 'button')
-    buttonContainer.append(button);
-    button.innerText = path.id;
-    path.style.cursor = 'pointer';
-    Lib.handlePathTitle(path, title);
-    Lib.setColorBehavior(path, button);
-  });
-}
+// Parse the SVG and add it to the page
+const parser: DOMParser = new DOMParser();
+const svgDoc: Document = parser.parseFromString(svg, 'image/svg+xml');
+const svgElement: HTMLElement = svgDoc.documentElement;
+mapContainer.appendChild(svgElement);
+
+// Filter and sort path from the SVG
+const paths: SVGElement[] = Array.from(svgElement.getElementsByTagNameNS('http://www.w3.org/2000/svg', 'path'));
+const filteredPaths: SVGElement[] = paths
+  .filter(path => !globals.staticPathNames.includes(path.id))
+  .sort((a, b) => a.id.localeCompare(b.id));
+
+// Add buttons and color behavior for each path
+const title: SVGTextElement = Lib.createText('white', 0.75, 10, 290, 'start', 'alphabetic');
+svgElement.appendChild(title);
+filteredPaths.forEach(path => {
+  const button = document.createElement('div');
+  button.setAttribute('class', 'button')
+  buttonContainer.append(button);
+  button.innerText = path.id;
+  path.style.cursor = 'pointer';
+  Lib.handlePathTitle(path, title);
+  Lib.setColorBehavior(path, button);
+});
