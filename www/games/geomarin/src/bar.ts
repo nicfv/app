59b197e3f5e6f2afe9bfe3b66d03d388e1245990
nicfv <github@nicfv.com>
2026-05-17 08:11:22 -0700

    comments

--- www/games/geomarin/src/bar.ts
+++ www/games/geomarin/src/bar.ts
@@ -9,9 +9,11 @@ export class Bar implements Drawable {
      */
     constructor(private readonly name: string, private readonly value: number, private readonly maxValue: number, private readonly barNum: number, private readonly thickness: number, private readonly color: string) { }
     public draw(ctx: CanvasRenderingContext2D): void {
+        // Calculate bar dimensions and position
         const yOffset: number = this.barNum * this.thickness * 1.25;
         const maxWidth: number = ctx.canvas.width - this.thickness * 5;
         const barWidth: number = this.value / this.maxValue * maxWidth;
+        // Draw the bar background
         ctx.fillStyle = this.color;
         ctx.fillRect(this.thickness * 1.5, yOffset - this.thickness / 2, barWidth + this.thickness * 0.5, this.thickness);
         // Draw the bar name and value
