6499e0e3545b2182e8784bc2b6f54343e1ded184
nicfv <github@nicfv.com>
2026-05-18 16:33:14 -0700

    determine close answers

--- www/games/geomarin/src/solution.ts
+++ www/games/geomarin/src/solution.ts
@@ -1,3 +1,4 @@
+import { borders } from '../assets/borders.json' with { type: 'json' };
 /**
  * The correct answer for this game
  */
@@ -5,4 +6,10 @@ export const correct = 'Bolinas';
 /**
  * Close but incorrect answers for this game
  */
-export const close: string[] = ['Sausalito', 'Tiburon', 'Stinson Beach'];
+export const close: string[] = [];
+// Determine close answers based on borders
+for (const border of borders) {
+    if (border.includes(correct)) {
+        close.push(...border.filter(region => region !== correct));
+    }
+}
