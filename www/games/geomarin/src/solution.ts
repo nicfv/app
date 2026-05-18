b24f80f3f5ee62c14cc50bd058f9200844b2ca75
nicfv <github@nicfv.com>
2026-05-18 16:35:34 -0700

    avoid dupes

--- www/games/geomarin/src/solution.ts
+++ www/games/geomarin/src/solution.ts
@@ -10,6 +10,10 @@ export const close: string[] = [];
 // Determine close answers based on borders
 for (const border of borders) {
     if (border.includes(correct)) {
-        close.push(...border.filter(region => region !== correct));
+        for (const region of border) {
+            if (region !== correct && !close.includes(region)) {
+                close.push(region);
+            }
+        }
     }
 }
