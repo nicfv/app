933c851a8d90c04e8737cbc85a8819dae9c3b101
Nic <github@nicfv.com>
2026-06-30 21:28:15 -0700

    fix non null assertion

--- www/games/revolution-idle/src/close-info-handler.ts
+++ www/games/revolution-idle/src/close-info-handler.ts
@@ -1,3 +1,3 @@
 const closeInfoButton: HTMLButtonElement = document.getElementById('close-info') as HTMLButtonElement;
 const gameInfoDiv: HTMLDivElement = document.getElementById('game-info') as HTMLDivElement;
-closeInfoButton.addEventListener('click', () => gameInfoDiv.parentElement!.removeChild(gameInfoDiv));
+closeInfoButton.addEventListener('click', () => gameInfoDiv.parentElement?.removeChild(gameInfoDiv));
