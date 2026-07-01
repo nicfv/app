0388a39e0db728d03c9f128f7bcec516de2b8c20
Nic <github@nicfv.com>
2026-06-30 21:35:09 -0700

    setup game

--- www/games/revolution-idle/src/index.ts
+++ www/games/revolution-idle/src/index.ts
@@ -1,1 +1,13 @@
 import './close-info-handler';
+import { Canvas } from 'graphico';
+
+const canv: Canvas = new Canvas({
+    background: 'black',
+    border: 'black',
+    borderBlur: 'gray',
+    width: 800,
+    height: 600,
+    parent: document.getElementById('game') as HTMLElement,
+});
+
+console.log(canv);
