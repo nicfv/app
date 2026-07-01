7323185502996f6dd67958177da058cdcda1fe5b
Nic <github@nicfv.com>
2026-06-30 21:15:40 -0700

    add close info functionality

--- www/games/revolution-idle/src/index.ts
+++ www/games/revolution-idle/src/index.ts
@@ -1,1 +1,3 @@
-console.log('loaded!');
+const closeInfoButton: HTMLButtonElement = document.getElementById('close-info') as HTMLButtonElement;
+const gameInfoDiv: HTMLDivElement = document.getElementById('game-info') as HTMLDivElement;
+closeInfoButton.addEventListener('click', () => gameInfoDiv.parentElement!.removeChild(gameInfoDiv));
