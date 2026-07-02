ffb2a31fa13aec74f606571396d39a22078245f6
nicfv <github@nicfv.com>
2026-07-01 17:10:51 -0700

    remove unnessesary escape char

--- www/games/revolution-idle/src/buy-button.ts
+++ www/games/revolution-idle/src/buy-button.ts
@@ -3,6 +3,6 @@ import { Wheel } from './wheel';
 
 export class BuyButton extends Button {
     constructor(private readonly wheel: Wheel) {
-        super(`${wheel.currentSpeedHz().toFixed(2)} \u2192 ${wheel.getNextNSpeed().toFixed(2)}Hz\n\$${wheel.getNextNCost().toFixed(2)}`, wheel.color, 10, 55 * wheel.index, 120, 35, () => console.log(`clicked ${wheel.index}`));
+        super(`${wheel.currentSpeedHz().toFixed(2)} \u2192 ${wheel.getNextNSpeed().toFixed(2)}Hz\n$${wheel.getNextNCost().toFixed(2)}`, wheel.color, 10, 55 * wheel.index, 120, 35, () => console.log(`clicked ${wheel.index}`));
     }
 }
