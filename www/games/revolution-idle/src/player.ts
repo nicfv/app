3914cee930f0adaeb3a8c6be3ed11ce820e193cb
nicfv <github@nicfv.com>
2026-07-02 15:57:52 -0700

    render black background

--- www/games/revolution-idle/src/player.ts
+++ www/games/revolution-idle/src/player.ts
@@ -4,6 +4,10 @@ import { FONT_FAMILY, FONT_SIZE } from './globals';
 export class Player implements Drawable {
     constructor(public money: number) { }
     public draw(graphics: CanvasRenderingContext2D): void {
+        // Render black background
+        graphics.fillStyle = 'black';
+        graphics.fillRect(0, graphics.canvas.height - FONT_SIZE * 3, graphics.canvas.width, FONT_SIZE * 3);
+        // Render white text (money)
         graphics.fillStyle = 'white';
         graphics.font = `bold ${FONT_SIZE * 2}px ${FONT_FAMILY}`;
         graphics.textAlign = 'center';
