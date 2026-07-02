e722e0ac9fd694db92aa59a8e8f7c3f32cf7ab2f
nicfv <github@nicfv.com>
2026-07-02 16:13:29 -0700

    start working on income class

---
+++ www/games/revolution-idle/src/income.ts
@@ -0,0 +1,7 @@
+import { Drawable } from 'graphico';
+
+export class Income implements Drawable {
+    public draw(graphics: CanvasRenderingContext2D): void {
+        throw new Error('Method not implemented.');
+    }
+}
