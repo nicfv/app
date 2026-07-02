c17f820eb007d9a8e02607e7c14c5cd6e2610559
nicfv <github@nicfv.com>
2026-07-02 15:58:17 -0700

    only draw buy buttons onscreen

--- www/games/revolution-idle/src/buy-button.ts
+++ www/games/revolution-idle/src/buy-button.ts
@@ -15,7 +15,11 @@ export class BuyButton extends Button {
         super.text = `${N(this.wheel.currentSpeedHz())} > ${N(this.wheel.getNextNSpeed())}Hz\n$${N(this.wheel.getNextNCost())}`;
     }
     public draw(graphics: CanvasRenderingContext2D): void {
-        this.y = (graphics.canvas.height - this.h) / 2 - 40 * (NUM_WHEELS - this.wheel.index - this.zoom.getZoom());
-        super.draw(graphics);
+        if (Math.abs(NUM_WHEELS - this.wheel.index - this.zoom.getZoom()) < 5) {
+            this.y = (graphics.canvas.height - this.h) / 2 - 45 * (NUM_WHEELS - this.wheel.index - this.zoom.getZoom());
+            super.draw(graphics);
+        } else {
+            this.y = -Infinity;
+        }
     }
 }
