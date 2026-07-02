33ef291adc434bd1dfd92bfdb82afd69ef44020c
nicfv <github@nicfv.com>
2026-07-01 17:14:05 -0700

    draw money

--- www/games/revolution-idle/src/player.ts
+++ www/games/revolution-idle/src/player.ts
@@ -1,7 +1,13 @@
 import { Drawable } from 'graphico';
+import { FONT_FAMILY, FONT_SIZE } from './globals';
 
 export class Player implements Drawable {
+    constructor(public money: number) { }
     public draw(graphics: CanvasRenderingContext2D): void {
-        throw new Error('Method not implemented.');
+        graphics.fillStyle = 'white';
+        graphics.font = `bold ${FONT_SIZE * 2}px ${FONT_FAMILY}`;
+        graphics.textAlign = 'center';
+        graphics.textBaseline = 'bottom';
+        graphics.fillText(`$${this.money.toFixed(2)}`, graphics.canvas.width / 2, graphics.canvas.height - FONT_SIZE / 2);
     }
 }
