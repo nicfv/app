ded5bf872aac5bc19a8eb406e8d3a3a2a8d94c52
nicfv <github@nicfv.com>
2026-07-02 17:25:38 -0700

    helper funcs and docs

--- www/games/revolution-idle/src/income.ts
+++ www/games/revolution-idle/src/income.ts
@@ -1,16 +1,41 @@
+import { SMath } from 'smath';
 import { Drawable } from 'graphico';
 import { FONT_SIZE } from './globals';
 import { Zoom } from './zoom';
 import { Wheel } from './wheel';
 import { WheelScore } from './wheel-score';
 
+/**
+ * Handles and renders game income.
+ */
 export class Income implements Drawable {
     private readonly wheelScores: WheelScore[];
+    /**
+     * Initialize a new income handler.
+     */
     constructor(private readonly zoom: Zoom, private readonly wheels: Wheel[]) {
         this.wheelScores = [];
         for (const wheel of wheels) {
             this.wheelScores.push(new WheelScore(wheel, 0));
         }
+    }
+    /**
+     * Calculate the income gained per complete rotation.
+     */
+    public getIncomePerRotation(): number {
+        return SMath.prod(this.wheelScores.map(ws => ws.getScore()));
+    }
+    /**
+     * Calculate the number of complete rotations per second.
+     */
+    public getRotationsPerSecond(): number {
+        return SMath.sum(this.wheels.map(w => w.currentSpeedHz()));
+    }
+    /**
+     * Calculate the average income gained per second.
+     */
+    public getIncomePerSecond(): number {
+        return this.getIncomePerRotation() * this.getRotationsPerSecond();
     }
     public draw(graphics: CanvasRenderingContext2D): void {
         graphics.fillStyle = 'black';
