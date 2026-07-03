adc0953485d250c38ea1c101324432f18ece52fd
nicfv <github@nicfv.com>
2026-07-02 17:55:21 -0700

    biggen font size

--- www/games/revolution-idle/src/player.ts
+++ www/games/revolution-idle/src/player.ts
@@ -6,10 +6,10 @@ export class Player implements Drawable {
     public draw(graphics: CanvasRenderingContext2D): void {
         // Render black background
         graphics.fillStyle = 'black';
-        graphics.fillRect(0, graphics.canvas.height - FONT_SIZE * 3, graphics.canvas.width, FONT_SIZE * 3);
+        graphics.fillRect(0, graphics.canvas.height - FONT_SIZE * 4, graphics.canvas.width, FONT_SIZE * 4);
         // Render white text (money)
         graphics.fillStyle = 'white';
-        graphics.font = `bold ${FONT_SIZE * 2}px ${FONT_FAMILY}`;
+        graphics.font = `bold ${FONT_SIZE * 3}px ${FONT_FAMILY}`;
         graphics.textAlign = 'center';
         graphics.textBaseline = 'bottom';
         graphics.fillText(`$${this.money.toFixed(2)}`, graphics.canvas.width / 2, graphics.canvas.height - FONT_SIZE / 2);
