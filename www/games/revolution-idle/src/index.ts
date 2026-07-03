031ed479d82c6ee97bd34a67250241074303083e
nicfv <github@nicfv.com>
2026-07-02 17:20:49 -0700

    actually render it

--- www/games/revolution-idle/src/index.ts
+++ www/games/revolution-idle/src/index.ts
@@ -5,6 +5,7 @@ import { NUM_WHEELS } from './globals';
 import { BuyButton } from './buy-button';
 import { Player } from './player';
 import { Zoom } from './zoom';
+import { Income } from './income';
 
 const player: Player = new Player(0);
 const zoom: Zoom = new Zoom(700, 150);
@@ -16,6 +17,8 @@ for (let i = 0; i < NUM_WHEELS; i++) {
     wheels[i].increaseSpeed();
     buttons.push(new BuyButton(wheels[i], zoom));
 }
+
+const income: Income = new Income(zoom, wheels);
 
 const canv: Canvas = new Canvas({
     background: 'black',
@@ -35,6 +38,7 @@ const canv: Canvas = new Canvas({
         }
         canv.draw(player);
         canv.draw(zoom);
+        canv.draw(income);
     },
     mousemove(x, y) {
         for (const bbtn of buttons) {
