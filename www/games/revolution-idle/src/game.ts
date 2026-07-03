e6c7eb7307cccc25ce8238e86981c1d0fe9637bf
nicfv <github@nicfv.com>
2026-07-02 18:17:08 -0700

    maybe we will use index for this...

--- www/games/revolution-idle/src/game.ts
+++
@@ -1,7 +0,0 @@
-import { Drawable } from 'graphico';
-
-export class Game implements Drawable {
-    public draw(graphics: CanvasRenderingContext2D): void {
-        throw new Error('Method not implemented.');
-    }
-}
