1722ef855ce0775146ab03322def05ddc630c5f3
nicfv <github@nicfv.com>
2026-07-24 13:15:28 -0700

    enable/disable and add comments

--- www/games/revolution-idle/src/asc-button.ts
+++ www/games/revolution-idle/src/asc-button.ts
@@ -5,9 +5,21 @@ import { N } from './lib';
 import { player, zoom } from './state';
 import { Wheel } from './wheel';
 
+/**
+ * Represents a button to ascend a specific wheel
+ */
 export class AscendButton extends Button {
+    /**
+     * The cost for ascending
+     */
     private ascAmount: number;
+    /**
+     * The amount of ascensions
+     */
     private ascensions: number;
+    /**
+     * Initialize a new ascension button
+     */
     constructor(x: number, width: number, private readonly wheel: Wheel) {
         super('', wheel.color, false, x, 0, width, Label.fontSize * 3, () => {
             player.money -= this.ascAmount;
@@ -16,13 +28,24 @@ export class AscendButton extends Button {
         this.ascAmount = 0;
         this.ascensions = 0;
     }
+    /**
+     * Update text and cost
+     */
     private update(): void {
+        // Determine the ascension level
         this.ascensions = 1;
         while (this.wheel.getNextNAscensionCost(this.ascensions + 1) <= player.money) {
             this.ascensions++;
         }
+        // Calculate and display cost
         this.ascAmount = this.wheel.getNextNAscensionCost(this.ascensions);
-        super.text = `Ascend +${this.ascensions}\n-${N(this.ascAmount)}`;
+        super.text = `Ascend +${this.ascensions}\n$${N(this.ascAmount)}`;
+        // Check if player has sufficient funds to ascend
+        if (player.money > this.ascAmount) {
+            super.enable();
+        } else {
+            super.disable();
+        }
     }
     public draw(graphics: CanvasRenderingContext2D): void {
         const centerDelta: number = NUM_WHEELS - this.wheel.index - zoom.getZoom();
