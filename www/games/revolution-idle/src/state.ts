c066d39a9ba09cabc771de74a9a13f26c67ef4a0
nicfv <github@nicfv.com>
2026-07-28 13:40:38 -0700

    reset positioning and start with $1

--- www/games/revolution-idle/src/state.ts
+++ www/games/revolution-idle/src/state.ts
@@ -5,9 +5,9 @@ import { Shop } from './shop';
 import { Wheel } from './wheel';
 import { Zoom } from './zoom';
 
-export const player: Player = new Player(1e14, 10);
-export const zoom: Zoom = new Zoom(700, 150);
-export const buyType: BuyType = new BuyType(690, 250);
+export const player: Player = new Player(1, 10);
+export const zoom: Zoom = new Zoom(700, 175);
+export const buyType: BuyType = new BuyType(685, 250);
 
 export const wheels: Wheel[] = [];
 for (let i = 0; i < player.getNumWheels(); i++) {
