7f0fee81f9c53324e95d814abf835d8c9d5eba73
nicfv <github@nicfv.com>
2026-07-31 14:24:06 -0700

    add main menu

--- www/games/revolution-idle/src/index.ts
+++ www/games/revolution-idle/src/index.ts
@@ -1,6 +1,6 @@
 import './close-info-handler';
 import { Canvas } from 'graphico';
-import { buyType, income, paused, player, shop, wheels, zoom } from './state';
+import { buyType, income, menu, paused, player, shop, wheels, zoom } from './state';
 
 const canv: Canvas = new Canvas({
     background: 'black',
@@ -20,16 +20,19 @@ const canv: Canvas = new Canvas({
         canv.draw(income);
         canv.draw(shop);
         canv.draw(buyType);
+        canv.draw(menu);
     },
     mousemove(x, y) {
         zoom.checkHover(x, y);
         buyType.checkHover(x, y);
         shop.checkHover(x, y);
+        menu.checkHover(x, y);
     },
     mousedown(button) {
         zoom.click(button);
         buyType.click(button);
         shop.click(button);
+        menu.click(button);
     },
     focus(dt) {
         for (const wheel of wheels) {
