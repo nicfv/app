0b20ecef4aefe21ee97eebbe910ec56cda37ffea
nicfv <github@nicfv.com>
2026-07-31 12:38:53 -0700

    add some menu buttons

--- www/games/revolution-idle/src/ctrl.ts
+++ www/games/revolution-idle/src/ctrl.ts
@@ -1,6 +1,17 @@
 import { Drawable } from 'graphico';
+import { Button } from './button';
+import { Color } from 'viridis';
 
 export class Control implements Drawable {
+    private static readonly btnColor: Color = new Color(200, 200, 200);
+    private readonly save: Button;
+    private readonly clear: Button;
+    private readonly help: Button;
+    constructor(private readonly x: number, private readonly y: number) {
+        this.save = new Button('Save', Control.btnColor, true, x, y, 50, 50, () => { });
+        this.clear = new Button('Reset', Control.btnColor, true, x, y, 50, 50, () => { });
+        this.help = new Button('Help', Control.btnColor, true, x, y, 50, 50, () => { });
+    }
     public draw(graphics: CanvasRenderingContext2D): void {
         throw new Error("Method not implemented.");
     }
