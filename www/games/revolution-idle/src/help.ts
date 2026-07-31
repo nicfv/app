ec377b512bf9b759fb475a66ce1d470d9acc2a4c
nicfv <github@nicfv.com>
2026-07-31 15:53:24 -0700

    show button blinking and tooltip

--- www/games/revolution-idle/src/help.ts
+++ www/games/revolution-idle/src/help.ts
@@ -1,23 +1,31 @@
-import { Drawable } from 'graphico';
-import { Label } from './label';
 import { Color } from 'viridis';
+import { Button } from './button';
+import { Label } from './label';
 
 /**
  * Represents a help message
  */
-export class Help extends Label implements Drawable {
+export class Help extends Button {
     /**
      * Highlight outline color
      */
-    private static readonly highlightStroke: Color = new Color(255, 255, 0);
+    private static readonly highlightStroke: Color = new Color(255, 255, 25);
     /**
      * Highlight fill color
      */
-    private static readonly highlightFill: Color = new Color(255, 255, 0, 10);
+    private static readonly highlightFill: Color = new Color(255, 255, 0, 25);
+    /**
+     * Full transparency
+     */
+    private static readonly transparent: Color = new Color(0, 0, 0, 0);
     /**
      * Highlight blink time in milliseconds
      */
     private static readonly highlightBlinkMS = 500;
+    /**
+     * Tooltip shown for help
+     */
+    private readonly tip: Label;
     /**
      * The current time for this help message
      */
@@ -25,27 +33,24 @@ export class Help extends Label implements Drawable {
     /**
      * Create a new help message with optional highlighted region.
      */
-    constructor(text: string, private readonly hx: number, private readonly hy: number, private readonly hw: number, private readonly hh: number) {
-        super(text, new Color(255, 255, 255), 1, false, 'center', 'top', 0, 0);
+    constructor(text: string, textOffsetX: number, textOffsetY: number, x: number, y: number, w: number, h: number, callback: () => void) {
+        super('', Help.highlightFill, true, x, y, w, h, () => callback());
+        this.tip = new Label(text, Help.highlightStroke, 1, false, 'left', 'top', x + textOffsetX, y + textOffsetY);
     }
     /**
      * Compute a single timestep for this help message
      */
     public tick(dt: number): void {
         this.time = (this.time + dt) % (Help.highlightBlinkMS * 2);
+        if (this.time > Help.highlightBlinkMS) {
+            super.color = Help.transparent;
+        } else {
+            super.color = Help.highlightFill;
+        }
     }
     public draw(graphics: CanvasRenderingContext2D): void {
-        // Make sure text is centered on page
-        super.x = graphics.canvas.width / 2;
-        super.y = (graphics.canvas.height - super.getHeight()) / 2;
+        // Render label and button as highlighted region
         super.draw(graphics);
-        // Blink a rectangular outline
-        if (this.time > Help.highlightBlinkMS) {
-            graphics.lineWidth = 2;
-            graphics.fillStyle = Help.highlightFill.toString();
-            graphics.strokeStyle = Help.highlightStroke.toString();
-            graphics.fillRect(this.hx, this.hy, this.hw, this.hh);
-            graphics.strokeRect(this.hx, this.hy, this.hw, this.hh);
-        }
+        this.tip.draw(graphics);
     }
 }
