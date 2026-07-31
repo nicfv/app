4f5df411dca2c8bbdebc37bdfa69f0830adca2d6
nicfv <github@nicfv.com>
2026-07-31 14:58:23 -0700

    menu in bottom right

--- www/games/revolution-idle/src/state.ts
+++ www/games/revolution-idle/src/state.ts
@@ -19,5 +19,5 @@ for (let i = 0; i < player.getNumWheels(); i++) {
 export const income: Income = new Income();
 export const shop: Shop = new Shop(10, 120);
 
-export const menu: Menu = new Menu(400, 200, 100);
+export const menu: Menu = new Menu(725, 400, 100);
 export const paused: Help = new Help('Revolution Idle 4 has lost focus!\nThe game is NOT paused, but has\nstopped rendering to save resources.\nThe game is still running in the background.');
