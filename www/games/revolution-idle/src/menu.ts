9bd572a1c6799cfc9d4ee87ad03950534f823a68
nicfv <github@nicfv.com>
2026-07-31 12:39:17 -0700

    rename to menu

--- www/games/revolution-idle/src/menu.ts
+++ www/games/revolution-idle/src/menu.ts
@@ -2,15 +2,15 @@ import { Drawable } from 'graphico';
 import { Button } from './button';
 import { Color } from 'viridis';
 
-export class Control implements Drawable {
+export class Menu implements Drawable {
     private static readonly btnColor: Color = new Color(200, 200, 200);
     private readonly save: Button;
     private readonly clear: Button;
     private readonly help: Button;
     constructor(private readonly x: number, private readonly y: number) {
-        this.save = new Button('Save', Control.btnColor, true, x, y, 50, 50, () => { });
-        this.clear = new Button('Reset', Control.btnColor, true, x, y, 50, 50, () => { });
-        this.help = new Button('Help', Control.btnColor, true, x, y, 50, 50, () => { });
+        this.save = new Button('Save', Menu.btnColor, true, x, y, 50, 50, () => { });
+        this.clear = new Button('Reset', Menu.btnColor, true, x, y, 50, 50, () => { });
+        this.help = new Button('Help', Menu.btnColor, true, x, y, 50, 50, () => { });
     }
     public draw(graphics: CanvasRenderingContext2D): void {
         throw new Error("Method not implemented.");
