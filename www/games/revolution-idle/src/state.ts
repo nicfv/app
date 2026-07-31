7ff7055df847bd5f8c117f3458f93849a2561a0c
nicfv <github@nicfv.com>
2026-07-31 15:37:53 -0700

    use pause class and start working on help guide

--- www/games/revolution-idle/src/state.ts
+++ www/games/revolution-idle/src/state.ts
@@ -2,6 +2,7 @@ import { BuyType } from './buy-type';
 import { Help } from './help';
 import { Income } from './income';
 import { Menu } from './menu';
+import { Pause } from './pause';
 import { Player } from './player';
 import { Shop } from './shop';
 import { Wheel } from './wheel';
@@ -20,4 +21,7 @@ export const income: Income = new Income();
 export const shop: Shop = new Shop(10, 120);
 
 export const menu: Menu = new Menu(725, 400, 100);
-export const paused: Help = new Help('Revolution Idle 4 has lost focus!\nThe game is NOT paused, but has\nstopped rendering to save resources.\nThe game is still running in the background.');
+export const paused: Pause = new Pause('Revolution Idle 4 has lost focus!\nThe game is NOT paused, but has\nstopped rendering to save resources.\nThe game is still running in the background.');
+export const help: Help[] = [
+    new Help('This is your in-game money\n(click to continue)', 50, 550, 700, 45),
+];
