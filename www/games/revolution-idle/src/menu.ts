63daafafb0b603519ddd7cc236455a5ae89681fd
nicfv <github@nicfv.com>
2026-07-31 16:40:10 -0700

    take in callbacks

--- www/games/revolution-idle/src/menu.ts
+++ www/games/revolution-idle/src/menu.ts
@@ -48,31 +48,60 @@ export class Menu implements Drawable {
      */
     private isOpen: boolean;
     /**
+     * Determines whether the tutorial should be shown
+     */
+    private isShowHelp = false;
+    /**
+     * The callback for saving data
+     */
+    private saveCallback = () => { return };
+    /**
+     * The callback for muting sound
+     */
+    private muteCallback = () => { return };
+    /**
+     * The callback for playing sound
+     */
+    private unmuteCallback = () => { return };
+    /**
+     * The callback for clearing all data
+     */
+    private clearCallback = () => { return };
+    /**
      * Create a new in-game menu
      */
     constructor(x: number, y: number, width: number) {
         this.isOpen = false;
         this.title = new Label('Menu', new Color(255, 255, 255), 1, false, 'center', 'bottom', x, y - Menu.btnPadding);
         this.save = new ToggleButton([
-            ['Save', Menu.btnColor, () => { return }],
+            ['Save', Menu.btnColor, () => { this.saveCallback(); }],
             ['Saved!', new Color(150, 250, 150), () => { return }],
         ], x - (width / 2), y, width, Menu.btnHeight);
         this.help = new ToggleButton([
-            ['Help', Menu.btnColor, () => { return }],
-            ['Hide Help', Menu.btnColor, () => { return }],
+            ['Help', Menu.btnColor, () => { this.isShowHelp = true; }],
+            ['Hide Help', Menu.btnColor, () => { this.isShowHelp = false; }],
         ], x - (width / 2), y + Menu.btnHeight + Menu.btnPadding, width, Menu.btnHeight);
         this.mute = new ToggleButton([
-            ['Mute', Menu.btnColor, () => { return }],
-            ['Unmute', Menu.btnColor, () => { return }],
+            ['Mute', Menu.btnColor, () => { this.muteCallback(); }],
+            ['Unmute', Menu.btnColor, () => { this.unmuteCallback(); }],
         ], x - (width / 2), y + (Menu.btnHeight + Menu.btnPadding) * 2, width, Menu.btnHeight);
         this.clear = new ToggleButton([
             ['Reset', Menu.btnColor, () => { return }],
-            ['Confirm', new Color(255, 0, 0), () => { return }],
+            ['Confirm', new Color(255, 0, 0), () => { this.clearCallback(); }],
         ], x - (width / 2), y + (Menu.btnHeight + Menu.btnPadding) * 3, width, Menu.btnHeight);
         this.back = new ToggleButton([
             ['Menu', Menu.btnColor, () => this.toggle()],
             ['Back', Menu.btnColor, () => this.toggle()],
         ], x - (width / 2), y + (Menu.btnHeight + Menu.btnPadding) * 4, width, Menu.btnHeight);
+    }
+    /**
+     * Set all callbacks, needed from the main canvas
+     */
+    public setCallbacks(save: () => void, mute: () => void, unmute: () => void, clear: () => void): void {
+        this.saveCallback = save;
+        this.muteCallback = mute;
+        this.unmuteCallback = unmute;
+        this.clearCallback = clear;
     }
     /**
      * Open or close the menu depending on its current state
@@ -81,6 +110,12 @@ export class Menu implements Drawable {
         this.isOpen = !this.isOpen;
         this.save.reset();
         this.clear.reset();
+    }
+    /**
+     * Return whether the tutorial should be displayed
+     */
+    public showHelp(): boolean {
+        return this.isShowHelp;
     }
     /**
      * Check if the mouse is currently hovering over any of the menu buttons
