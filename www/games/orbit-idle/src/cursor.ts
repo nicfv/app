c1fc9d9c8d02f0be9f9e60bf2820e09a27e08b2d
nicfv <github@nicfv.com>
2026-08-02 10:25:48 -0700

    add props

--- www/games/orbit-idle/src/cursor.ts
+++ www/games/orbit-idle/src/cursor.ts
@@ -1,6 +1,24 @@
 import { Drawable } from 'graphico';
 
+/**
+ * Represents the user's cursor
+ */
 export class Cursor implements Drawable {
+    /**
+     * X-coordinate [px]
+     */
+    private x: number;
+    /**
+     * Y-coordinate [px]
+     */
+    private y: number;
+    /**
+     * Create a new instance of a cursor
+     */
+    constructor() {
+        this.x = 0;
+        this.y = 0;
+    }
     public draw(graphics: CanvasRenderingContext2D): void {
         throw new Error("Method not implemented.");
     }
