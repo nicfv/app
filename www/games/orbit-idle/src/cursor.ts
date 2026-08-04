2b01d26ebd428db5f23e0a8176c894bc2872837c
nicfv <github@nicfv.com>
2026-08-03 17:29:13 -0700

    add more class methods

--- www/games/orbit-idle/src/cursor.ts
+++ www/games/orbit-idle/src/cursor.ts
@@ -5,6 +5,14 @@ import { Drawable } from 'graphico';
  */
 export class Cursor implements Drawable {
     /**
+     * Star radius
+     */
+    private static readonly lgR = 4;
+    /**
+     * Planet radius
+     */
+    private static readonly smR = 2;
+    /**
      * X-coordinate [px]
      */
     private x: number;
@@ -12,14 +20,33 @@ export class Cursor implements Drawable {
      * Y-coordinate [px]
      */
     private y: number;
+    private planetAngle: number;
     /**
      * Create a new instance of a cursor
      */
     constructor() {
         this.x = 0;
         this.y = 0;
+        this.planetAngle = 0;
+    }
+    /**
+     * Move the cursor when the user moves the mouse
+     */
+    public move(x: number, y: number): void {
+        this.x = x;
+        this.y = y;
+    }
+    public tick(dt: number): void {
+        this.planetAngle = (this.planetAngle + dt) % (2 * Math.PI);
     }
     public draw(graphics: CanvasRenderingContext2D): void {
-        throw new Error("Method not implemented.");
+        graphics.fillStyle = 'white';
+        graphics.beginPath();
+        graphics.arc(this.x, this.y, Cursor.lgR, 0, 2 * Math.PI);
+        graphics.fill();
+        graphics.fillStyle = 'red';
+        graphics.beginPath();
+        graphics.arc(this.x + (Cursor.lgR + Cursor.smR), this.y, Cursor.smR, 0, 2 * Math.PI);
+        graphics.fill();
     }
 }
