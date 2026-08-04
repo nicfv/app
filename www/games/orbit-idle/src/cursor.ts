b95c5a79aea030457e42c0b772f973b8ce2e9d69
nicfv <github@nicfv.com>
2026-08-03 17:32:32 -0700

    calculate small planet pos

--- www/games/orbit-idle/src/cursor.ts
+++ www/games/orbit-idle/src/cursor.ts
@@ -1,4 +1,5 @@
 import { Drawable } from 'graphico';
+import { Vec3 } from 'smath';
 
 /**
  * Represents the user's cursor
@@ -13,40 +14,41 @@ export class Cursor implements Drawable {
      */
     private static readonly smR = 2;
     /**
-     * X-coordinate [px]
+     * Mouse coordinates
      */
-    private x: number;
+    private pos: Vec3;
     /**
-     * Y-coordinate [px]
+     * The angle of the orbiting planet
      */
-    private y: number;
     private planetAngle: number;
     /**
      * Create a new instance of a cursor
      */
     constructor() {
-        this.x = 0;
-        this.y = 0;
+        this.pos = new Vec3(0, 0);
         this.planetAngle = 0;
     }
     /**
      * Move the cursor when the user moves the mouse
      */
     public move(x: number, y: number): void {
-        this.x = x;
-        this.y = y;
+        this.pos = new Vec3(x, y);
     }
+    /**
+     * Rotate the small planet
+     */
     public tick(dt: number): void {
         this.planetAngle = (this.planetAngle + dt) % (2 * Math.PI);
     }
     public draw(graphics: CanvasRenderingContext2D): void {
         graphics.fillStyle = 'white';
         graphics.beginPath();
-        graphics.arc(this.x, this.y, Cursor.lgR, 0, 2 * Math.PI);
+        graphics.arc(this.pos.x, this.pos.y, Cursor.lgR, 0, 2 * Math.PI);
         graphics.fill();
+        const planetPos: Vec3 = this.pos.plus(Vec3.fromPolar(Cursor.lgR + Cursor.smR, this.planetAngle));
         graphics.fillStyle = 'red';
         graphics.beginPath();
-        graphics.arc(this.x + (Cursor.lgR + Cursor.smR), this.y, Cursor.smR, 0, 2 * Math.PI);
+        graphics.arc(planetPos.x, planetPos.y, Cursor.smR, 0, 2 * Math.PI);
         graphics.fill();
     }
 }
