e8ff6805f1c725be9ffc83f488f9a301d339b64c
nicfv <github@nicfv.com>
2026-08-07 16:44:10 -0700

    add visibility and close button

--- www/games/orbit-idle/src/stats.ts
+++ www/games/orbit-idle/src/stats.ts
@@ -1,24 +1,60 @@
 import { Drawable } from 'graphico';
 import { Label } from './label';
 import { Color } from 'viridis';
+import { Button } from './button';
 
 /**
  * Represents an in-game statistics panel
  */
 export class Statistics implements Drawable {
     private static readonly shade: Color = new Color(0, 0, 0, 50);
+    private visible: boolean;
     private readonly title: Label;
     private readonly headers: Label;
     private readonly content: Label;
+    private readonly close: Button;
     /**
      * Create a new statistics window
      */
     constructor() {
+        this.visible = false;
         this.title = new Label('Orbit Idle: Statistics', new Color(255, 255, 255), 2, true, 'center', 'bottom', 0, Label.fontSize * 14);
         this.headers = new Label('Test', new Color(255, 255, 255), 1, false, 'right', 'top', 0, Label.fontSize * 15);
         this.content = new Label('Test', new Color(255, 255, 255), 1, true, 'left', 'top', 0, Label.fontSize * 15);
+        this.close = new Button('x', new Color(255, 100, 100), true, Label.fontSize / 2, Label.fontSize / 2, Label.fontSize * 2, Label.fontSize * 2, null, () => this.hide());
+    }
+    /**
+     * Show the statistics panel
+     */
+    public show(): void {
+        this.visible = true;
+    }
+    /**
+     * Hide the statistics panel
+     */
+    private hide(): void {
+        this.visible = false;
+    }
+    /**
+     * Check to see if the mouse is hovering over the close button
+     */
+    public checkHover(x: number, y: number): void {
+        if (this.visible) {
+            this.close.checkHover(x, y);
+        }
+    }
+    /**
+     * Check to see if the user clicked the close button
+     */
+    public click(button: number): void {
+        if (this.visible) {
+            this.close.click(button);
+        }
     }
     public draw(graphics: CanvasRenderingContext2D): void {
+        if (!this.visible) {
+            return;
+        }
         graphics.fillStyle = Statistics.shade.toString();
         graphics.fillRect(0, 0, graphics.canvas.width, graphics.canvas.height);
         this.title.x = graphics.canvas.width / 2;
@@ -27,5 +63,6 @@ export class Statistics implements Drawable {
         this.title.draw(graphics);
         this.headers.draw(graphics);
         this.content.draw(graphics);
+        this.close.draw(graphics);
     }
 }
