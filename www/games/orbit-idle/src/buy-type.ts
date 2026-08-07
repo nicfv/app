e486cf2b42d4fd81592125781e3206a637acfeab
nicfv <github@nicfv.com>
2026-08-07 15:56:27 -0700

    no hotkeys

--- www/games/orbit-idle/src/buy-type.ts
+++ www/games/orbit-idle/src/buy-type.ts
@@ -29,9 +29,9 @@ export class BuyType implements Drawable {
      */
     constructor(x: number, y: number) {
         this.quantity = 1;
-        this.buy1 = new Button('1', BuyType.btnColor, true, x - BuyType.btnSize * 3 / 2 - BuyType.btnPad, y, BuyType.btnSize, BuyType.btnSize, () => this.quantity = 1);
-        this.buy10 = new Button('10', BuyType.btnColor, true, x - BuyType.btnSize / 2, y, BuyType.btnSize, BuyType.btnSize, () => this.quantity = 10);
-        this.buy100 = new Button('100', BuyType.btnColor, true, x + BuyType.btnSize / 2 + BuyType.btnPad, y, BuyType.btnSize, BuyType.btnSize, () => this.quantity = 100);
+        this.buy1 = new Button('1', BuyType.btnColor, true, x - BuyType.btnSize * 3 / 2 - BuyType.btnPad, y, BuyType.btnSize, BuyType.btnSize, null, () => this.quantity = 1);
+        this.buy10 = new Button('10', BuyType.btnColor, true, x - BuyType.btnSize / 2, y, BuyType.btnSize, BuyType.btnSize, null, () => this.quantity = 10);
+        this.buy100 = new Button('100', BuyType.btnColor, true, x + BuyType.btnSize / 2 + BuyType.btnPad, y, BuyType.btnSize, BuyType.btnSize, null, () => this.quantity = 100);
         this.label = new Label('', new Color(255, 255, 255), 1, false, 'center', 'bottom', x, y - Label.fontSize);
     }
     /**
