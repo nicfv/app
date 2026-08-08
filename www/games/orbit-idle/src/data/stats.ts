9decb8a5311349fb34e927bfcc3c348f3b9e2303
nicfv <github@nicfv.com>
2026-08-07 18:01:22 -0700

    extend that

--- www/games/orbit-idle/src/data/stats.ts
+++ www/games/orbit-idle/src/data/stats.ts
@@ -3,12 +3,13 @@ import { Label } from '../form-controls/label';
 import { Color } from 'viridis';
 import { Button } from '../form-controls/button';
 import { player } from './state';
-import { N } from './lib';
+import { N, SaveLoad } from './lib';
+import { SMath } from 'smath';
 
 /**
  * Represents an in-game statistics panel
  */
-export class Statistics implements Drawable {
+export class Statistics extends SaveLoad<StatsData> implements Drawable {
     private static readonly shade: Color = new Color(0, 0, 0, 50);
     private visible: boolean;
     private readonly title: Label;
@@ -18,12 +19,21 @@ export class Statistics implements Drawable {
     /**
      * Create a new statistics window
      */
-    constructor(public stats: Stats = defaultStats) {
+    constructor(stats: StatsData = defaultStats) {
+        super(stats);
         this.visible = false;
         this.title = new Label('Orbit Idle: Statistics', new Color(255, 255, 255), 2, true, 'center', 'bottom', 0, Label.fontSize * 14);
         this.headers = new Label('Started on\nSolar system size\nTotal earned\nTotal spent\nTotal planetary years\nSpeed levels purchased\nAscensions purchased\n\nCredits\n\n\nDisclaimer', new Color(255, 255, 255), 1, false, 'right', 'top', 0, Label.fontSize * 15);
         this.content = new Label('', new Color(255, 255, 255), 1, true, 'left', 'top', 0, Label.fontSize * 15);
         this.close = new Button('x', new Color(255, 100, 100), true, Label.fontSize / 2, Label.fontSize / 2, Label.fontSize * 2, Label.fontSize * 2, null, () => this.hide());
+    }
+    public load(data: StatsData): void {
+        super.data.startTime = SMath.clamp(data.startTime, 0, Infinity) | 0;
+        super.data.moneyEarned = SMath.clamp(data.moneyEarned, 0, Infinity);
+        super.data.moneySpent = SMath.clamp(data.moneySpent, 0, Infinity);
+        super.data.orbitsCompleted = SMath.clamp(data.orbitsCompleted, 0, Infinity) | 0;
+        super.data.speedLevelsPurchased = SMath.clamp(data.speedLevelsPurchased, 0, Infinity) | 0;
+        super.data.ascensionsPurchased = SMath.clamp(data.ascensionsPurchased, 0, Infinity) | 0;
     }
     /**
      * Show the statistics panel
@@ -62,7 +72,7 @@ export class Statistics implements Drawable {
         graphics.fillStyle = Statistics.shade.toString();
         graphics.fillRect(0, 0, graphics.canvas.width, graphics.canvas.height);
         // Set content
-        this.content.value = `${new Date(this.stats.startTime).toLocaleString()}\n${N(player.getNumOrbits())} planets\n$${N(this.stats.moneyEarned)}\n$${N(this.stats.moneySpent)}\n${N(this.stats.orbitsCompleted)} orbits\n${N(this.stats.speedLevelsPurchased)}\n${N(this.stats.ascensionsPurchased)}\n\nNicolas Ventura\nnicfv.com\n\nGenerative AI was used for\n30-second looping audio but\nnot for game design or code`;
+        this.content.value = `${new Date(super.data.startTime).toLocaleString()}\n${N(player.getNumOrbits())} planets\n$${N(super.data.moneyEarned)}\n$${N(super.data.moneySpent)}\n${N(super.data.orbitsCompleted)} orbits\n${N(super.data.speedLevelsPurchased)}\n${N(super.data.ascensionsPurchased)}\n\nNicolas Ventura\nnicfv.com\n\nGenerative AI was used for\n30-second looping audio but\nnot for game design or code`;
         // X-center all labels
         this.title.x = graphics.canvas.width / 2;
         this.headers.x = (graphics.canvas.width - Label.fontSize) / 2;
@@ -78,7 +88,7 @@ export class Statistics implements Drawable {
 /**
  * Interface containing various in-game statistics
  */
-export interface Stats {
+export interface StatsData {
     startTime: number;
     moneyEarned: number;
     moneySpent: number;
@@ -87,7 +97,7 @@ export interface Stats {
     ascensionsPurchased: number;
 }
 
-const defaultStats: Stats = {
+const defaultStats: StatsData = {
     startTime: 0,
     moneyEarned: 0,
     moneySpent: 0,
