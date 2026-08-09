37339afb75b7ab2c5e67a56b1d33b89938c669e3
nicfv <github@nicfv.com>
2026-08-09 15:03:50 -0700

    v1.0.0

--- www/games/orbit-idle/src/data/state.ts
+++ www/games/orbit-idle/src/data/state.ts
@@ -12,7 +12,7 @@ import { Tutorial } from '../form-controls/tutorial';
 import { Zoom } from '../ui/zoom';
 
 // Global constants
-export const version = 'v1.3.0-alpha';
+export const version = 'v1.0.0';
 
 // UI elements
 export const cursor: Cursor = new Cursor();
